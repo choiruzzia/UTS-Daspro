83f05bd2a97583277ef1f71733fd8ba73d8637b5
sintaaaaa <74360616+sintaaaaa@users.noreply.github.com>
2020-12-11 00:27:32 +0700

    Menambahkan elif status sampai selesai

--- gaji karyawan.py
+++ gaji karyawan.py
@@ -22,3 +22,23 @@ if Status=='Staff':
         print('Gaji total =Rp.',Gatot)
     else:
         print("Input salah")
+elif Status=='Honor':
+    Gaji = 1000000
+    if Gol=='A':
+        Bonus=200000
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    elif Gol=='B':
+        Bonus=300000
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    elif Gol=='C':
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    else:
+        print("Input salah")
+else:
+    print("Input salah")
