6d027ca9b9db5b58172b9f7c9629f38eb549476f
choiruzzia <74298878+choiruzzia@users.noreply.github.com>
2020-12-10 22:53:24 +0700

    Create gaji karyawan.py
    line 1 - line 24

---
+++ gaji karyawan.py
@@ -0,0 +1,24 @@
+Gatot = Gaji+Bonus
+Nama = input("Nama Karyawan : ")
+NIK = input("NIK Karyawan : ")
+Status = input("Status (Staff/Honor) : ")
+Gol = input("Golongan (A/B/C) : ")
+if Status=='Staff':
+    Gaji = 2000000
+    if Gol=='A':
+        Bonus=500000
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    elif Gol=='B':
+        Bonus=600000
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    elif Gol=='C':
+        Bonus=700000
+        print('Gaji =Rp.',Gaji)
+        print('Bonus=Rp.',Bonus)
+        print('Gaji total =Rp.',Gatot)
+    else:
+        print("Input salah")
